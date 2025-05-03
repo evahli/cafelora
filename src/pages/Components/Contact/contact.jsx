0c0c61ae73cc30ef5cc672bae24ae41a40dd50d0
Eva Nunez <evahlivkova@MacBook-Pro-2.local>
2025-05-03 10:44:11 +0200

    navigation for narrow screen and added ID tags sections of the website

--- src/pages/Components/Contact/contact.jsx
+++ src/pages/Components/Contact/contact.jsx
@@ -1,7 +1,7 @@
 import './contact.css'
 
 export const Contact = () => (
-  <section>
+  <section id='contact'>
         <div className="container">
           <h2>Kde nás najdete</h2>
           <div className="contact">
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,7 @@
 import { render } from '@czechitas/render';
 import '../global.css';
 import './index.css';
-import './Components/Footer/footer'
+import './Components/Footer/footer';
 import { Footer } from './Components/Footer/footer';
 import { Contact } from './Components/Contact/contact';
 import { Gallery } from './Components/Gallery/gallery';
@@ -13,17 +13,28 @@ document.querySelector('#root').innerHTML = render(
   <div className="page">
     <Header />
     <main>
-    <Banner />
-    <Menu />
-    <Gallery />
-    <Contact />
+      <Banner />
+      <Menu />
+      <Gallery />
+      <Contact />
     </main>
     <Footer />
-  </div>
+  </div>,
 );
 
+const navButton = document.querySelector('.nav-btn');
+const navElement = document.querySelector('.rollout-nav');
+navButton.addEventListener('click', () => {
+  navElement.classList.toggle('nav-closed');
+});
+
+navElement.addEventListener('click', (event) => {
+  console.log(event.target);
+  navElement.classList.add('nav-closed');
+});
 
 
-/* Header - hlavička stránky,
-Banner - uvítací obrázek,
-*/ 
+/* 
+
+
+*/
