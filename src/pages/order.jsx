4564273ed9fa01ae06f9a5005adecb737a093027
Eva Nunez <evahlivkova@MacBook-Pro-2.local>
2025-05-03 17:09:21 +0200

    final step for displaying order detail

--- src/pages/order.jsx
+++ src/pages/order.jsx
@@ -6,7 +6,9 @@ import { Header } from './Components/Header/header';
 import { Footer } from './Components/Footer/footer';
 import { Order } from './Components/Order/order';
 
-const response = await fetch('http://localhost:4000/api/drinks?filter=ordered:eq:true&select=id,name,image');
+const response = await fetch(
+  'http://localhost:4000/api/drinks?filter=ordered:eq:true&select=id,name,image',
+);
 const json = await response.json();
 
 document.querySelector('#root').innerHTML = render(
@@ -17,15 +19,16 @@ document.querySelector('#root').innerHTML = render(
       <main className="order">
         <div className="container order__content">
           <h1>Vaše objedávnka</h1>
-          
-          {json.data.length === 0 ? <p className="empty-order">Zatím nemáte nic objednáno</p> : <Order items={json.data}/>}
-            
+
+          {json.data.length === 0 ? (
+            <p className="empty-order">Zatím nemáte nic objednáno</p>
+          ) : (
+            <Order items={json.data} />
+          )}
         </div>
       </main>
 
       <Footer />
     </div>
-  </div>
+  </div>,
 );
-
-
